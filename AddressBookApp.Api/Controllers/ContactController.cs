b7cbf4a2ed5a29659cb3684b6528570abcf012b2
Rogue Elephant <jup.trandell@gmail.com>
2020-03-21 16:09:01 +0000

    Added initial post and get methods

--- AddressBookApp.Api/Controllers/ContactController.cs
+++ AddressBookApp.Api/Controllers/ContactController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AddressBookApp.Api.DataAccess.DomainModels;
+using AddressBookApp.DataAccess;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,29 +14,50 @@ namespace AddressBookApp.Api.Controllers
     [Route("api/[controller]")]
     public class ContactsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly Contact[] Contacts = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            new Contact
+            {
+                FirstName = "John",
+                Surname = "Smith",
+                DateOfBirth = DateTime.Parse("1/1/1975"),
+                Email = "foo@bar.com"
+            }
         };
 
         private readonly ILogger<ContactsController> _logger;
+        private readonly AddressBookDataContext _dbContext;
 
-        public ContactsController(ILogger<ContactsController> logger)
+        public ContactsController(ILogger<ContactsController> logger, AddressBookDataContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        // [HttpGet]
-        // public IEnumerable<Contact> Get()
-        // {
-        //     var rng = new Random();
-        //     return Enumerable.Range(1, 5).Select(index => new Contact
-        //     {
-        //         Date = DateTime.Now.AddDays(index),
-        //         TemperatureC = rng.Next(-20, 55),
-        //         Summary = Summaries[rng.Next(Summaries.Length)]
-        //     })
-        //     .ToArray();
-        // }
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return Contacts;
+        }
+
+        [HttpGet()]
+        [Route("{email}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetByEmail(string email)
+        {
+            var foundContact = _dbContext.Contacts.Find(email);
+
+            return Ok(foundContact);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _dbContext.Contacts.Add(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetByEmail), new { email = contact.Email }, contact);
+        }
     }
 }
