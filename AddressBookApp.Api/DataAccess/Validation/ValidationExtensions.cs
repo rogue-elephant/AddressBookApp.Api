8175123a7f9a9dfc93fa7ded2dec53f078bc9a37
Rogue Elephant <jup.trandell@gmail.com>
2020-03-26 10:04:17 +0000

    Some summary tag documentation

--- AddressBookApp.Api/DataAccess/Validation/ValidationExtensions.cs
+++ AddressBookApp.Api/DataAccess/Validation/ValidationExtensions.cs
@@ -5,11 +5,30 @@ namespace AddressBookApp.DataAccess.Validation
 {
     public static class ValidationExtensions
     {
+        /// <summary>
+        /// Adds an error code and message to the validation step.
+        /// </summary>
+        /// <param name="IRuleBuilderOptions<TEntity"></param>
+        /// <param name="rule"></param>
+        /// <param name="errorCode">The error code to use for the validation step.</param>
+        /// <param name="propertyName">The property being validated - user friendly name for displaying in error message.</param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <returns></returns>
         public static IRuleBuilderOptions<TEntity, TProperty> AddError<TEntity,TProperty>(this IRuleBuilderOptions<TEntity, TProperty> rule,
         ErrorCode errorCode, string propertyName = null)
             => rule
                 .WithErrorCode(errorCode.GetErrorCodeString())
                 .WithMessage(errorCode.GetErrorMessage(propertyName));
+        
+        /// <summary>
+        /// Validates a name property.
+        /// </summary>
+        /// <param name="IRuleBuilder<TEntity"></param>
+        /// <param name="rule"></param>
+        /// <param name="propertyName">The property being validated - user friendly name for displaying in error message.</param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
         public static IRuleBuilderOptions<TEntity, string> NameValidation<TEntity>(this IRuleBuilder<TEntity, string> rule, string propertyName = null)
         {
             return rule
@@ -20,6 +39,16 @@ namespace AddressBookApp.DataAccess.Validation
                 
                 // TODO: Could add regex for ensuring names but this can cause some problems down line... Tekashi 6ix9ine for example....
         }
+
+        /// <summary>
+        /// Adds validation to ensure the property is not left blank - null or empty.
+        /// </summary>
+        /// <param name="IRuleBuilder<TEntity"></param>
+        /// <param name="rule"></param>
+        /// <param name="propertyName">The property being validated - user friendly name for displaying in error message.</param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <returns></returns>
          public static IRuleBuilderOptions<TEntity, TProperty> NotNullOrEmpty<TEntity,TProperty>(this IRuleBuilder<TEntity, TProperty> rule,
          string propertyName = null)
         {
